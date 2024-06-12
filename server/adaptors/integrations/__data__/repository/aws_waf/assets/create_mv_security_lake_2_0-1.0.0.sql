d0075e5ae91a45a5263f0c08da05d1ef385b5b4c
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-06-12 00:14:53 -0700

    updated mv queries
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- server/adaptors/integrations/__data__/repository/aws_waf/assets/create_mv_security_lake_2_0-1.0.0.sql
+++ server/adaptors/integrations/__data__/repository/aws_waf/assets/create_mv_security_lake_2_0-1.0.0.sql
@@ -1,18 +1,18 @@
 CREATE MATERIALIZED VIEW {table_name}__mview AS
 SELECT
     CAST(FROM_UNIXTIME(`time`/ 1000) AS TIMESTAMP) AS `@timestamp`,
-    CAST(IFNULL(`metadata.product.version`, 'Unknown') AS STRING) AS `aws.waf.formatVersion`,
-    CAST(IFNULL(`metadata.product.feature.uid`, 'Unknown') AS STRING) AS `aws.waf.webaclId`,
-    CAST(IFNULL(`firewall_rule.uid`, 'Unknown') AS STRING) AS `aws.waf.terminatingRuleId`,
-    CAST(IFNULL(`firewall_rule.type`, 'Unknown') AS STRING) AS `aws.waf.terminatingRuleType`,
+    CAST(IFNULL(metadata.product.version, 'Unknown') AS STRING) AS `aws.waf.formatVersion`,
+    CAST(IFNULL(metadata.product.feature.uid, 'Unknown') AS STRING) AS `aws.waf.webaclId`,
+    CAST(IFNULL(firewall_rule.uid, 'Unknown') AS STRING) AS `aws.waf.terminatingRuleId`,
+    CAST(IFNULL(firewall_rule.type, 'Unknown') AS STRING) AS `aws.waf.terminatingRuleType`,
     CAST(IFNULL(action, 'Unknown') AS STRING) AS `aws.waf.action`,
-    CAST(IFNULL(`src_endpoint.svc_name`, 'Unknown') AS STRING) AS `aws.waf.httpSourceName`,
-    CAST(IFNULL(`src_endpoint.uid`, 'Unknown') AS STRING) AS `aws.waf.httpSourceId`,
-    ruleGroupList AS `aws.waf.ruleGroupList`,
+    CAST(IFNULL(src_endpoint.svc_name, 'Unknown') AS STRING) AS `aws.waf.httpSourceName`,
+    CAST(IFNULL(src_endpoint.uid, 'Unknown') AS STRING) AS `aws.waf.httpSourceId`,
+    unmapped['ruleGroupList'] AS `aws.waf.ruleGroupList`,
     unmapped['rateBasedRuleList'] AS `aws.waf.rateBasedRuleList`,
-    nonTerminatingMatchingRules AS `aws.waf.nonTerminatingMatchingRules`,
-    CAST(IFNULL(`http_status`, 0) AS LONG) AS `aws.waf.responseCodeSent`,
-    httpRequest AS `aws.waf.httpRequest`,
+    unmapped['nonTerminatingMatchingRules'] AS `aws.waf.nonTerminatingMatchingRules`,
+    CAST(IFNULL(http_status, 0) AS LONG) AS `aws.waf.responseCodeSent`,
+    unmapped['httpRequest'] AS `aws.waf.httpRequest`,
     metadata.labels AS `aws.waf.labels`,
     unmapped['captchaResponse'] AS `aws.waf.captchaResponse`
 FROM
